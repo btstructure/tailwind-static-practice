c7e6039d88ccb695511059808512a45b8558d534
Beyhan <btstructure97@gmail.com>
2023-11-02 18:54:03 -0400

    save

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Newsletter() {
+  return (
+    <div className="w-full py-16 text-white">
+      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
+        <div>
+            <h1>Want tips & tricks to optimize your flow?</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Newsletter;
