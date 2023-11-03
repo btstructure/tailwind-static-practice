ba7a2cfdb00b418ac682e320d5a29c03ccbd0565
Beyhan <btstructure97@gmail.com>
2023-11-03 19:49:19 -0400

    complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,7 @@ function Navbar() {
   };
   return (
     <div className="flex justify-between  items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">
-        REACT.
-      </h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
       <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
@@ -20,9 +18,15 @@ function Navbar() {
         <li className="p-4">Contact</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {!nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}
       </div>
-      <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
+      <div
+        className={
+          !nav
+            ? "fixed left-[-100%]"
+            : "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+        }
+      >
         <h1 className="w-full text-3xl font-bold m-4 text-[#00df9a]">REACT.</h1>
         <ul className="uppercase">
           <li className="p-4 border-b border-gray-600">Home</li>
